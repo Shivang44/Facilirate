7d7e96344821044ce409254abb4848921d28707e
hbernie830 <bernhardt.42@osu.edu>
2017-11-17 11:58:18 -0500

    Added the table for buildings and a could items in it

--- facilirate/app/controllers/home_controller.rb
+++ facilirate/app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
     # index(get types, get buildings, insert into html dropdown boxes)
   def index
       @types = FacilityType.all
+      @buildings = Building.all
   end
 end
--- facilirate/db/schema.rb
+++ facilirate/db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114183824) do
+ActiveRecord::Schema.define(version: 20171117164241) do
+
+  create_table "buildings", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "facility_types", force: :cascade do |t|
     t.text     "ftype"
