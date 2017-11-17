24931b8879c1958acd79649f43f5329320083a2c
hbernie830 <bernhardt.42@osu.edu>
2017-11-17 12:24:19 -0500

    Got a basic table check set up and also set the guidlines for how to return correct reviews

--- facilirate/app/controllers/result_controller.rb
+++ facilirate/app/controllers/result_controller.rb
@@ -1,5 +1,26 @@
 class ResultController < ApplicationController
   def showResult
-    @args = [params[:facility], params[:building], params[:room]]
+    @args = []
+
+    #I know not correct, just messing with tables
+    if(params[:facility].length > 0) then
+        @args.push(FacilityType.where('ftype = ?', params[:facility]).first )
+    end
+
+    if(params[:building].length > 0) then
+        @args.push(Building.where('name = ?', params[:building]).first )
+    end
+    #@args = [params[:facility], params[:building], params[:room]]
+
+
+    #Steps:
+    #Get all reviews into an array
+    #If a ftype was given:
+        #Keep only those that are of the right ftype, remove the result
+    #If a building given:
+        #Keep only those that are of the right name, remove the result
+    #If a room number given:
+        #Keep only those that are of the right number, remove the result
+
   end
 end
