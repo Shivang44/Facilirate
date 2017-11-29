eda34aa705e828b38a01f8c9a07021d07fd89d2a
draju1 <dilr7017@gmail.com>
2017-11-29 16:46:49 -0500

    Fixed edit button

--- facilirate/app/controllers/facility_controller.rb
+++ facilirate/app/controllers/facility_controller.rb
@@ -1,9 +1,5 @@
 class FacilityController < ApplicationController
-    # FQDM.com/facility/id
-    # params it takes: id of room
-    # what it returns: AVG review/rating, room/building name, and individual reviews
-    def show
-    end
+
 
     # Returns a blank form to create a review for a facility
     # Written by Shivang Saxena on 11/26/2017
@@ -68,5 +64,59 @@ class FacilityController < ApplicationController
 
         # Redirect user to resuts page
         redirect_to controller: 'home', action: 'index'
+    end
+    def editReview
+      # TODO: Do a server-side validation of all params in addition to client side (html) validation
+          # Also sanitize input -- escape any javascript and html
+      # TODO: Add validations/constrictions to DB
+      @building = params[:building]
+      @room = params[:room]
+      @facilityType = params[:facility]
+      @review = params[:review]
+      @rating = params[:rating]
+
+      @newReview = Review.find_by_id(params[:id])
+
+      @newReview.review = @review
+      @newReview.rating = @rating
+      @newReview.save
+
+      roomId = Room.where(roomNum: @room).first.id
+      # Get the number of reviews for this room so we can calculate the new average
+      reviews = Review.where(room_id: roomId).to_a
+      nReviews = reviews.length
+      reviewSum = reviews.reduce(0) { |sum, review| sum + review.rating}
+      newAvg = reviewSum / nReviews
+
+      # Update rating average for room
+      currentRoom = Room.find(roomId)
+      currentRoom.avgRating = newAvg
+      currentRoom.save
+
+      # Redirect user to results page
+      redirect_to controller: 'home', action: 'index'
+    end
+
+    def show
+      if !user_signed_in?
+          redirect_to controller: 'user', action: 'notLoggedIn'
+      else
+          # Get all building names in ascending order
+          @buildings = Building.all.order(name: :asc).to_a.map {
+              |building| building.name
+          }
+
+          # Same with facility types and room numbers
+          @facilitytypes = FacilityType.all.order(ftype: :asc).to_a.map {
+              |facilityType| facilityType.ftype
+          }
+
+          @rooms = Room.all.order(roomNum: :asc).to_a.map {
+              |room| room.roomNum
+          }
+      end
+      @id = params[:id]
+    end
+    def edit
     end
 end
