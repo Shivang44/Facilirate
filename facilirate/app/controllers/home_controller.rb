d3a49d9682bfa88799b91c5beb494afd6001d357
hbernie830 <bernhardt.42@osu.edu>
2017-11-29 13:50:22 -0500

    Made an lot of CSS adjustments

--- facilirate/app/controllers/home_controller.rb
+++ facilirate/app/controllers/home_controller.rb
@@ -2,8 +2,8 @@ class HomeController < ApplicationController
     # index(get types, get buildings, insert into html dropdown boxes)
     # Hunter Bernhardt
   def index
-      @types = FacilityType.all
-      @buildings = Building.all
+      @types = FacilityType.all.to_a.sort! { |a, b| a.ftype <=> b.ftype }
+      @buildings = Building.all.to_a.sort! { |a, b| a.name <=> b.name }
   end
 
 end
