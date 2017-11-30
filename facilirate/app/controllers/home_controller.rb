9c13bf64e61879a6fbb208c2f731c0c34dd191d5
hbernie830 <bernhardt.42@osu.edu>
2017-11-30 12:58:21 -0500

    Added contributions to the project

--- facilirate/app/controllers/home_controller.rb
+++ facilirate/app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
     # index(get types, get buildings, insert into html dropdown boxes)
-    # Hunter Bernhardt
+    # Written by Hunter Bernhardt
   def index
       @types = FacilityType.all.to_a.sort! { |a, b| a.ftype <=> b.ftype }
       @buildings = Building.all.to_a.sort! { |a, b| a.name <=> b.name }
